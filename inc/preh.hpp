1b68cae70e40f628b5fc1b799fbc37420684ccdc
Wanxuan Li <lzlu1018@gmail.com>
2022-11-26 05:44:14 -0500

    start refine project

---
+++ inc/preh.hpp
@@ -0,0 +1,24 @@
+#ifndef PREH_H
+#define PREH_H
+
+// standard IO
+#include <iostream>
+#include <iomanip>
+
+// timer and random number
+#include <time.h>
+#include <chrono>
+
+// data structure
+#include <vector>
+#include <list>
+#include <map>
+
+// math and algorithm
+#include <cmath>
+#include <algorithm>
+
+// other
+#include <cassert>
+
+#endif
